304b98388191807a97d35ecbdbbff00b52793631
Anastasia Merz <anastasiamerz07@gmail.com>
2025-06-17 19:49:55 +0200

    lesson-28

--- Lesson-28/src/Client.java
+++ Lesson-28/src/Client.java
@@ -10,29 +10,29 @@ import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 7);
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             Scanner scanner = new Scanner(System.in);
 
-            new Thread(() ->{
+            new Thread(() -> {
                 try {
-                while (true){
-                String serverMes = dataInputStream.readUTF();
-                System.out.println("Server: " + serverMes);
-                    if (serverMes.equals("/end")) break;
+                    while (true) {
+                        String serverMes = dataInputStream.readUTF();
+                        System.out.println("Server: " + serverMes);
+                        if (serverMes.equals("/end")) break;
+                    }
+                } catch (IOException e) {
+                    System.out.println("Ошибка приёма от сервера: " + e.getMessage());
                 }
-            }catch (IOException e){
-                System.out.println("Ошибка приёма от сервера: " + e.getMessage());
-            }
             }).start();
-            while(true) {
+            while (true) {
                 String userInput = scanner.nextLine();
                 System.out.println("received: " + userInput);
                 dataOutputStream.writeUTF(userInput);
-                if(userInput.equals("/end"))
+                if (userInput.equals("/end"))
                     break;
             }
             socket.close();
